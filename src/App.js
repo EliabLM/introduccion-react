c9c108433b59f72ee5934f2f2220520eb999a917
eliablm <eliablopez@hotmail.com>
2021-09-07 22:52:08 -0500

    Agrega estructura principal de los componentes

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+// import './App.css';
+
+const todos = [
+	{ text: 'Cortar cebolla', completed: false },
+	{ text: 'Tomar el curso de introduccion a React', completed: false },
+	{ text: 'Llorar con la llorona', completed: false },
+];
+
+function App() {
+	return (
+		<>
+			<TodoCounter />
+
+			<TodoSearch />
+
+			<TodoList>
+				{todos.map((todo) => (
+					<TodoItem key={todo.text} text={todo.text} />
+				))}
+			</TodoList>
+
+			<CreateTodoButton />
+		</>
+	);
+}
+
+export default App;
